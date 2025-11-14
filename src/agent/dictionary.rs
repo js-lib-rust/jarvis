f37eee9f18cbaa65fcfdf62c91c7f7ec57887a06
Iulian Rotaru <mr.iulianrotaru@gmail.com>
2025-11-14 20:12:18 +0200

    Refine markdown format for dictionary agent response.

--- src/agent/dictionary.rs
+++ src/agent/dictionary.rs
@@ -15,6 +15,7 @@ pub struct DictionaryAgent;
 struct Definition {
     word: String,
     key: String,
+    part_of_speech: Option<String>,
     meanings: Vec<Meaning>,
     expressions: Vec<Expression>,
 }
@@ -62,34 +63,42 @@ impl DictionaryAgent {
 
             let mut cursor = collection.find(filter, options).await?;
             let mut response = Vec::<String>::new();
-            let mut numbering = 1;
             while let Some(result) = cursor.next().await {
-                 match result {
+                match result {
                     Ok(definition) => {
-                        response.push(format!("# {}", definition.word));
-                        response.push(String::new());
+                        if !definition.meanings.is_empty() {
+                            response.push(String::new());
+                            response.push(match definition.part_of_speech {
+                                Some(part_of_speech) => format!("# {}, {}", definition.word, part_of_speech.to_lowercase()),
+                                None => format!("# {}", definition.word),
+                            });
+                            response.push(String::new());
 
-                        for meaning in definition.meanings {
-                            response.push(format!("{}. {}", numbering, meaning.definition));
-                                response.push(String::new());
+                            let mut numbering = 1;
+                            for meaning in definition.meanings {
+                                response.push(format!("{}. {}", numbering, meaning.definition));
+                                    response.push(String::new());
 
-                            for example in meaning.examples {
-                                response.push(format!("> {}  ", example.text));
-                                if let Some(source) = example.source {
-                                    response.push(format!("\u{2014} _{}_", source));
+                                for example in meaning.examples {
+                                    response.push(format!("> {}  ", example.text));
+                                    if let Some(source) = example.source {
+                                        response.push(format!("\u{2014} _{}_", source));
+                                    }
+                                    response.push(String::new());
                                 }
-                                response.push(String::new());
+                                numbering = numbering + 1;
                             }
-                            numbering = numbering + 1;
+
+                            if !definition.expressions.is_empty() {
+                                response.push(String::from("## În expresie"));
+                            }
                         }
 
+                        for expression in &definition.expressions {
+                            response.push(format!("- __{}__: {}", expression.phrase, expression.definition));
+                        }
                         if !definition.expressions.is_empty() {
                             response.push(String::new());
-                            response.push(String::from("## În expresie"));
-                        }
-
-                        for expression in definition.expressions {
-                            response.push(format!("- __{}__: {}", expression.phrase, expression.definition));
                         }
                     }
                     Err(e) => eprintln!("Error loading document: {}", e),
